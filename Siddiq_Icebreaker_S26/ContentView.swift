d55241e9ce105a562d000b7a4772497b632bc2f1
MustafaSiddiq010 <mustafamech010@gmail.com>
2026-02-04 15:00:26 -0500

    Initial Commit

---
+++ Siddiq_Icebreaker_S26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Siddiq_Icebreaker_S26
+//
+//  Created by Mohammed Mustafa Siddiq on 2/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Siddiq_Icebreaker_S26/Siddiq_Icebreaker_S26App.swift
@@ -0,0 +1,17 @@
+//
+//  Siddiq_Icebreaker_S26App.swift
+//  Siddiq_Icebreaker_S26
+//
+//  Created by Mohammed Mustafa Siddiq on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Siddiq_Icebreaker_S26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
